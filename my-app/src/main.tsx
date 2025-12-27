1e008acb806cb98074fb4342a9b6757145d3ea55
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-12-27 11:01:36 +0900

    ヘッダーのためにページファイル作ったり

--- my-app/src/main.tsx
+++ my-app/src/main.tsx
@@ -3,12 +3,12 @@ import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App.tsx';
-import { SignupPage } from './pages/SignupPage.tsx';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <SignupPage />
+      <App />
     </BrowserRouter>
   </StrictMode>,
 );
