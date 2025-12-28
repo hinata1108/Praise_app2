e32f2d88138590c1d3f807db88ba6c9969a4bf8d
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-12-28 23:33:08 +0900

    createページの途中

--- my-app/src/pages/CreatePage.tsx
+++ my-app/src/pages/CreatePage.tsx
@@ -1,10 +1,34 @@
+import {createPost} from '../features/posts/posts.api';
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
 export function CreatePage() {
+    const navigate = useNavigate();
+    const [content ,setContent] =useState("")
+    const [error,setError] = useState<string | null>(null);
+    const handleSubmit = async (e:React.FormEvent) => {
+      e.preventDefault();
+      try{
+        await createPost(content);
+        navigate("/home");
+      }
+      catch(err:unknown){
+        if(err instanceof Error){
+          setError(err.message);
+        }else{
+          setError("投稿に失敗しました");
+        }
+      }
+    }
   return (
     <div>
-      <h2>Create Page</h2>
-      <p>This is the Create Page.</p>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <form onSubmit={handleSubmit}>
+        <h1>投稿作成</h1>
+   <input type="text" value={content} onChange={(e) => setContent(e.target.value)} required/>   
+   <button type="submit" >投稿する</button>
+      </form>
     </div>
   );
 }
