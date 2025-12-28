fce772e4130d2acb3a87e36683c1769d1734a42f
hinata1108 <240561028@ccmailg.meijo-u.ac.jp>
2025-12-28 18:19:32 +0900

    headerおしゃれになった

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import './Header.css';
 import { Link } from 'react-router-dom';
 import { useAuth } from '../features/auth/auth.hooks';
 import { signOut } from '../features/auth/auth.api';
@@ -8,24 +8,26 @@ export const Header = () => {
 
     if (loading) return <div>Loading...</div>;
 
-    return (
-        <div className="header">
-            <header>
-                <h1>My App Header</h1>
+    return ( 
+            <header className="header-modern">
+                <div className='container'>
+                <h1>ほめほめアプリ</h1>
+                <div className='nav-wrapper'>
                 <nav>
                     {user ?(
                         <>
                     <ul>
-                        <li><Link to="/mypost">My Posts</Link></li>
-                        <li><Link to="/create">Create</Link></li>
-                        <li><Link to="/home">Home</Link></li>
-                        <button onClick={signOut}>Logout</button>
+                        <li><Link to="/mypost">自分の投稿</Link></li>
+                        <li><Link to="/create">投稿を作成</Link></li>
+                        <li><Link to="/home">みんなの投稿</Link></li>
+                        <button onClick={signOut}>ログアウト</button>
                     </ul>
                     </>
                     ):(<></>)
                 }
                 </nav>
+                </div>
+                </div>
             </header>
-        </div>
     );
 }
